e41afea53ed21842bdcc4e8f00ca24fb7e8eda69
aagam-kala2005 <aagam.23fe10cai00359@muj.manipal.edu>
2025-04-12 16:31:23 +0530

    Update default_predictor.py

--- models/default_predictor.py
+++ models/default_predictor.py
@@ -1,1 +1,11 @@
+import joblib
+import pandas as pd
+
+model = joblib.load("models/default_predictor.pkl")
+
+def predict_default(user_data):
+    # user_data = {total_debt, income, age, payment_history_score}
+    input_df = pd.DataFrame([user_data])
+    prediction = model.predict(input_df)[0]
+    return "High Risk" if prediction == 1 else "Low Risk"
 
